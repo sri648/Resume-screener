12ce2062f2a15c050123dd36897b060b2656aa28
sri648 <deepachidhambaram@gmail.com>
2026-09-04 17:29:54 +0530

    Add vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:5174","http://localhost:5175"],
+    allow_origins=["http://localhost:5173","http://localhost:5174","https://resume-screener-khaki.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
